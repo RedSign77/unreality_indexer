26a847f227d8a93c853425e46310cdc65dc1537e
Zoltán Németh <signred@gmail.com>
2022-01-24 13:20:15 +0100

    Add new command: Remove unused attribute options

---
+++ Indexer/Console/Command/RemoveUAOptionsCommand.php
@@ -0,0 +1,124 @@
+<?php
+/*
+ * Copyright © Unreality One. All rights reserved.
+ */
+
+declare(strict_types = 1);
+
+namespace Unreality\Indexer\Console\Command;
+
+use Magento\Framework\Exception\LocalizedException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Magento\Framework\App\State;
+use Symfony\Component\Console\Helper\Table;
+use Magento\Framework\Console\Cli;
+use Magento\Framework\App\Area;
+use Unreality\Indexer\Ui\DataProvider\AttributeOptions\Listing\CollectionFactory;
+use Unreality\Indexer\Ui\DataProvider\AttributeOptions\Listing\Collection;
+
+class RemoveUAOptionsCommand extends Command
+{
+
+    /**
+     * @var State
+     */
+    private State $appState;
+
+    /**
+     * @var Collection
+     */
+    private $collection;
+
+    /**
+     * RemoveUnusedAttributeOptionsCommand constructor.
+     *
+     * @param CollectionFactory $collectionFactory
+     * @param State             $appState
+     */
+    public function __construct(
+        CollectionFactory $collectionFactory,
+        State $appState
+    ) {
+        /** @var Collection collection */
+        $this->collection = $collectionFactory->create(['mainTable' => 'eav_attribute_option_value']);
+        $this->appState   = $appState;
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('catalog:product:attribute:options:cleanup');
+        $this->setDescription('Removes unused product attribute options.');
+        parent::configure();
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws LocalizedException
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->setDecorated(true);
+        $this->appState->setAreaCode(Area::AREA_GLOBAL);
+        $attributeOptionValues = $this->collection->getData();
+        $optionIdsForeDelete   = $this->renderTable($output, $attributeOptionValues);
+
+        $helper   = $this->getHelper('question');
+        $question = new ConfirmationQuestion('Continue to remove attribute values? (y/n) ', false);
+
+        if (!$helper->ask($input, $output, $question)) {
+            return Cli::RETURN_SUCCESS;
+        }
+
+        try {
+            $connection = $this->collection->getConnection();
+            $table      = $this->collection->getTable('eav_attribute_option');
+            $connection->beginTransaction();
+            $connection->delete($table, ['option_id in (?)' => $optionIdsForeDelete]);
+            $connection->commit();
+
+            $output->writeln('');
+            $output->writeln('<info>Product attribute option values successfully cleaned up.</info>');
+        } catch (\Exception $exception) {
+            $output->writeln('');
+            $output->writeln("<error>{$exception->getMessage()}</error>");
+
+            return Cli::RETURN_FAILURE;
+        }
+
+        return Cli::RETURN_SUCCESS;
+    }
+
+    /**
+     * @param  OutputInterface $output
+     * @param  array           $attributeOptionValues
+     * @return array
+     */
+    private function renderTable(OutputInterface $output, array $attributeOptionValues): array
+    {
+        $optionIdsForeDelete = [];
+        $rows                = [];
+        $table               = new Table($output);
+        $table->setHeaders(['ID', 'Option ID', 'Value', 'Attribute Code']);
+        foreach ($attributeOptionValues as $value) {
+            $rowData               = [
+                'ID'             => $value['value_id'],
+                'Option ID'      => $value['option_id'],
+                'Value'          => $value['value'],
+                'Attribute Code' => $value['attribute_code'],
+            ];
+            $rows[]                = $rowData;
+            $optionIdsForeDelete[] = $value['option_id'];
+        }
+
+        $table->addRows($rows);
+        $table->render();
+        return $optionIdsForeDelete;
+    }
+}
---
+++ Indexer/Ui/DataProvider/AttributeOptions/Listing/Collection.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * Copyright © Unreality One. All rights reserved.
+ */
+
+declare(strict_types = 1);
+
+namespace Unreality\Indexer\Ui\DataProvider\AttributeOptions\Listing;
+
+use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
+use Magento\Catalog\Api\Data\ProductAttributeInterface;
+
+class Collection extends SearchResult
+{
+    /**
+     * Override _initSelect to add custom columns
+     *
+     * @return Collection
+     */
+    protected function _initSelect()
+    {
+        parent::_initSelect();
+        $this->getSelect()->joinInner(
+            ['eao' => $this->getTable('eav_attribute_option')],
+            '`main_table`.option_id = `eao`.option_id',
+            'attribute_id'
+        )->joinInner(
+            ['ea' => $this->getTable('eav_attribute')],
+            '`eao`.attribute_id = `ea`.attribute_id',
+            'attribute_code'
+        )->joinInner(
+            ['eet' => $this->getTable('eav_entity_type')],
+            '`eet`.entity_type_id = `ea`.entity_type_id',
+            ''
+        );
+        $this->addFieldToFilter('eet.entity_type_code', ['eq' => ProductAttributeInterface::ENTITY_TYPE_CODE]);
+        $this->addFieldToFilter('ea.backend_type', ['eq' => 'int']);
+        $this->addFieldToFilter('main_table.option_id', ['nin' => $this->getUsedOptionValueIds()]);
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    private function getUsedOptionValueIds(): array
+    {
+        $idsSelect = clone $this->getSelect();
+        $idsSelect->reset();
+        $idsSelect->from($this->getTable('eav_attribute_option'), 'attribute_id')
+                  ->group('attribute_id');
+        $attributeIds = $this->getConnection()->fetchCol($idsSelect);
+        $idsSelect->reset();
+        $idsSelect->from($this->getTable('catalog_product_entity_int'), 'value')
+                  ->where('attribute_id IN (?)', $attributeIds)
+                  ->group('value');
+
+        return $this->getConnection()->fetchCol($idsSelect);
+    }
+}
